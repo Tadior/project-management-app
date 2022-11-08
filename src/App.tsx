83eb32709253e7ebc4fed04e18aa80376aac4cee
Dmitriy Zamulin <zamulin1998@gmail.com>
2022-11-08 21:33:29 +0300

    init: create base structure

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Example from './components/Example/Example';
+import '../src/mainStyles/main.scss';
+
+function App() {
+  return <div className="App">{<Example />}</div>;
+}
+
+export default App;
---
+++ src/components/Example/Example.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import "./Example.scss";
+const Example = () => {
+  return (
+    <div className="example">
+      <div className="example__container">
+        <div className="example__content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quia, ad non iste id ex
+          sequi modi molestias, error nemo, eos praesentium fugiat? Laudantium, labore explicabo!
+          Impedit quas aspernatur doloremque? Architecto, eius ratione commodi et quaerat aperiam
+          expedita fuga dolorum cupiditate a illum tenetur fugit ad assumenda at similique in
+          repudiandae repellat enim! Velit harum sapiente neque ipsum nulla dolor.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Example;
