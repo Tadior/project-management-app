0cb7b775a6e88b98a8958d0a4a28abfdc9b80141
Ilona <mitsunaki@yandex.ru>
2022-11-12 14:11:36 +0300

    feat: add teamSection

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CreationSection from './components/CreationSection';
 import PromoSection from './components/PromoSection';
+import TeamSection from './components/teamSection';
 
 const WelcomePage = () => {
   return (
     <>
       <PromoSection />
       <CreationSection />
+      <TeamSection />
     </>
   );
 };
