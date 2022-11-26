d609296c6dfcc10cfb7dbbc2f8a7ce745b596a99
Ilona <mitsunaki@yandex.ru>
2022-11-26 15:11:38 +0300

    feat: add toast to signIn and signUp routes

--- src/pages/WelcomePage/components/PromoSection.tsx
+++ src/pages/WelcomePage/components/PromoSection.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import MagicBtn from './magicBtn';
 import Wizard from '../../../assets/images/welcome_image.png';
 import { useTranslation } from 'react-i18next';
+import { getCookie } from '../../../helper/Helper';
 
 const PromoSection = () => {
   const { t } = useTranslation();
+  const token = getCookie('token');
   return (
     <section className="promo">
       <div className="container">
@@ -15,7 +17,7 @@ const PromoSection = () => {
             {t('welcome_promo_title_p2')}
           </h1>
           <p className="promo__info-details section-text">{t('welcome_promo_text')}</p>
-          <MagicBtn to="/signUp" text={t('welcome_promo_btn')} />
+          <MagicBtn to={token ? '/projects' : '/signUp'} text={t('welcome_promo_btn')} />
         </div>
         <img src={Wizard} className="promo__img" alt="Wizard" />
       </div>
